23c48fa3ee5a232e729c03117d68f93ac43ec767
Damaris Virtan <virtan.alexandra@gmail.com>
2021-08-30 18:51:07 +0300

    feat: notifications for update

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const express = require('express')
-// const ExpressCache = require('express-cache-middleware')
-// const cacheManager = require('cache-manager')
- 
-// const cacheMiddleware = new ExpressCache(
-//     cacheManager.caching({
-//         store: 'memory', max: 10000, ttl: 100
-//     })
-// )
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors')
 const fetch = require("node-fetch");
 const app = express()
-var morgan = require('morgan')
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-//cacheMiddleware.attach(app)
 const BASE_API_URL = "http://localhost:8081";
 const WorkItem = require('./models/workItem')
 const LinkComponents = require('./models/linkComponents')
@@ -31,7 +20,6 @@ const issueServiceUrl = baseUrl + ':8084';
 const usersServiceUrl = baseUrl + ':8082';
 const commentsServiceUrl = baseUrl + ':8091';
 const projectServiceUrl = baseUrl + ':8083';
-const workItemServiceUrl = baseUrl + ':8084';
 const port = 8084
 
 var mongoDB = 'mongodb+srv://cata:cata@cluster0.wcbqw.mongodb.net/first?retryWrites=true&w=majority';
@@ -113,9 +101,9 @@ function groupBy(objectArray, property) {
       var key = obj[property];
       if (!acc[key]) {
         if(key==="Backlog"){
-            acc[key] = {items: [], id: 0,closed:false};
+            acc[key] = {items: [], id: 0};
         }else{
-            acc[key] = {items: [], id: obj.sprint_id,closed:obj.sprint_closed};
+            acc[key] = {items: [], id: obj.sprint_id};
         }
       }
       acc[key].items.push(obj);
@@ -124,135 +112,6 @@ function groupBy(objectArray, property) {
 
     return res;
   }
-
-  app.post('/allLinkedComponents', async (req, res) =>{
-    let result = [];
-    if (req.body.length) {
-        for (let index = 0; index < req.body.length; index++) {
-            if(req.body[index]!==''){
-                const rezComponent = await LinkComponents.find({ 'work_item': req.body[index] })
-                result.push(rezComponent[0]);
-            }else{
-                result.push([]);
-            }
-            
-        }
-    }
-    res.json(result)
-})
-app.post('/allLinkedLabels', async (req, res) =>{
-    let result = [];
-    if (req.body.length) {
-        for (let index = 0; index < req.body.length; index++) {
-            if(req.body[index]!==''){
-                const rezLabel = await LinkLabels.find({ 'work_item': req.body[index] })
-                result.push(rezLabel[0]);
-            }else{
-                result.push([]);
-            }
-            
-        }
-    }
-    res.json(result)
-})
-  var issues = [] 
-  var priorities = []
-  var sprints=[]
-  var epiclinks=[]
-  var assignees=[]
-  var comments=[]
-  var labels=[]
-  var components=[]
-async function requestsMicroservices(issueIds,priorityIds,sprintIds,epicLinkIds,assigneeIds,workItemsIds){
-   
- fetch(issuesServiceUrl + '/allIssues', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(issueIds)
-    })
-    .then(res => res.json())
-    .then(data => issues = data);
-
- fetch(priorityServiceUrl + '/allPriorities', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(priorityIds)
-    })
-    .then(res => res.json())
-    .then(data => priorities = data);
-    
- fetch(sprintServiceUrl + '/allsprints', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(sprintIds)
-    })
-    .then(res => res.json())
-    .then(data => sprints = data);
- fetch(issueServiceUrl + '/allepiclinks', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(epicLinkIds)
-    })
-    .then(res => res.json())
-    .then(data => epiclinks = data);
-
- fetch(usersServiceUrl + '/allusersselected', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(assigneeIds)
-    })
-    .then(res => res.json())
-    .then(data => assignees = data);
-    fetch(commentsServiceUrl + '/allcomments', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(workItemsIds)
-    })
-    .then(res => res.json())
-    .then(data => comments = data);
-
-
-    fetch(workItemServiceUrl + '/allLinkedComponents', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(workItemsIds)
-    })
-    .then(res => res.json())
-    .then(data => components = data);
-
-    fetch(workItemServiceUrl + '/allLinkedLabels', 
-    { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(workItemsIds)
-    })
-    .then(res => res.json())
-    .then(data => labels = data);
-
-}
  
 app.post('/workItemProject', async (req, res) =>{
     let result = []
@@ -263,10 +122,68 @@ app.post('/workItemProject', async (req, res) =>{
     epicLinkIds=record.map(i => i.epic_link);
     assigneeIds=record.map(i => i.assignee);
     sprintIds=record.map(i => i.sprint);
-    workItemIds=record.map(i => i._id);
 
-    let microservicesInfo=await requestsMicroservices(issueIds,priorityIds,sprintIds,epicLinkIds,assigneeIds)
+    issues = [] 
+    priorities = []
+    sprints=[]
+    epiclinks=[]
+    assignees=[]
+    await fetch(issuesServiceUrl + '/allIssues', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(issueIds)
+    })
+    .then(res => res.json())
+    .then(data => issues = data);
 
+    await fetch(priorityServiceUrl + '/allPriorities', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(priorityIds)
+    })
+    .then(res => res.json())
+    .then(data => priorities = data);
+    
+    await fetch(sprintServiceUrl + '/allsprints', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(sprintIds)
+    })
+    .then(res => res.json())
+    .then(data => sprints = data);
+
+    await fetch(issueServiceUrl + '/allepiclinks', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(epicLinkIds)
+    })
+    .then(res => res.json())
+    .then(data => epiclinks = data);
+
+    await fetch(usersServiceUrl + '/allusersselected', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(assigneeIds)
+    })
+    .then(res => res.json())
+    .then(data => assignees = data);
+
+    
     for (let index = 0; index < record.length; index++) {
         if (!issues[index]) {
             issues[index] = { name: "no issue" };
@@ -274,6 +191,23 @@ app.post('/workItemProject', async (req, res) =>{
         if (!priorities[index]) {
             priorities[index] = { name: "no priority" };
         }
+
+        let components = await LinkComponents.find({ work_item: record[index]._id });
+        let labels = await LinkLabels.find({ work_item: record[index]._id });
+
+        comments=[]
+    await fetch(commentsServiceUrl + '/allItemComments', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'_id':record[index]._id})
+    })
+    .then(res => res.json())
+    .then(data => comments = data);
+
+
         const componentDTO = {
             _id: record[index]._id,
             project: "projectId",
@@ -288,11 +222,10 @@ app.post('/workItemProject', async (req, res) =>{
             epic_link: epiclinks[index].name,
             sprint: sprints[index].name,
             sprint_id: sprints[index]._id,
-            sprint_closed:sprints[index].closed,
-            labels: labels[index],
-            components: components[index],
+            labels: labels,
+            components: components,
             status:record[index].status,
-            comments:comments[index],
+            comments:comments,
             positionInSprint:record[index].positionInSprint,
             positionInStatus:record[index].positionInStatus,
         }
@@ -305,32 +238,12 @@ app.post('/workItemProject', async (req, res) =>{
             hasBacklog=1
     }
     if(hasBacklog===0)
-        grouped["Backlog"]={items: [], id: 0,closed:false}
-
-    allProjectSprints=[]
-    await fetch(sprintServiceUrl + '/sprint/projectid', 
-        { 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({'id':req.body.id})
-        })
-        .then(res => res.json())
-        .then(data => allProjectSprints = data);
-        allProjectSprints.map((s)=>{
-            let exists=sprints.find(e=> e._id===s._id)
-            if(exists===undefined)
-                grouped[s.name]={items: [], id: s._id,closed:s.closed}
-        })
+        grouped["Backlog"]={items: [], id: 0}
     res.json(grouped)
 })
 
-
 app.get('/workItem', async (req, res) =>{
     const record= await WorkItem.find({})
-
-
     let result=[]
     for (let index = 0; index < record.length; index++) {
       let components = await LinkComponents.find({ work_item: record[index]._id });
@@ -370,13 +283,30 @@ app.get('/workItem/epic', async (req, res) =>{
 
 app.put('/updateWorkItem', async (req, res) =>{
     const newObject = req.body
+    let summary = req.body.summary.substring(0,15)
+    if(req.body.summary.length>15)
+    {
+        summary=summary+"..."
+    }
     const editedObject={project:newObject.project,issue_type:newObject.issue_type, epic_name:newObject.epic_name, summary:newObject.summary,description:newObject.description,priority:newObject.priority,linked_issue:newObject.linked_issue,issue:newObject.issue,assignee:newObject.assignee,epic_link:newObject.epic_link,sprint:newObject.sprint,status:newObject.status }
     const filter={_id:req.body._id}
     let update_= await WorkItem.findOneAndUpdate(filter, editedObject, {
         new: true,
         upsert: true 
       });
-
+      var request = {
+        project: "PRJ1",
+        message: "A workItem with the key: "+ update_.key + " - " +summary + " was modified !"
+      };
+    await fetch("http://localhost:1106/Project", 
+      { 
+          method: 'POST',
+          mode: "cors",
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(request)
+      })
     res.send(update_)
 })
 
@@ -427,6 +357,7 @@ app.post('/workItemById', async (req, res) =>{
 
 app.put('/workItemChangePositionStatus', async (req, res) =>{
   
+    console.log(req.body.items)
     await Promise.all(req.body.items.map(async(wi) => {  
     const editedObject = { _id:wi._id,positionInStatus:wi.positionInStatus}
     const filter={_id:wi._id}
